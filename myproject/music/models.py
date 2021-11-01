44f2392f23168740685c4184dd8a58bcdd88206c
SpencerMuniz <sbottomley44@gmail.com>
2021-11-01 11:04:18 -0700

    initial

---
+++ myproject/music/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Song(models.Model):
+    title = models.CharField(max_length=50)
+    artist = models.CharField(max_length=50)
+    album = models.CharField(max_length=50)
+    release_date = models.DateTimeField()
