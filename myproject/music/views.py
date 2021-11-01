2c53f92de98a83346e6876adcd897cbfee93a63f
SpencerMuniz <sbottomley44@gmail.com>
2021-11-01 12:36:56 -0700

    created detail class with get object, get, put, and delete method

--- myproject/music/views.py
+++ myproject/music/views.py
@@ -1,3 +1,4 @@
+from django.http import request
 from django.shortcuts import render
 from .models import Song
 from .serializers import SongSerializer
@@ -18,3 +19,28 @@ class SongList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class DetailClass(APIView):
+    def get_object(self, pk):
+        try:
+            return Song.objects.get(pk=pk)
+        except Song.DoesNotExist:
+            raise status.HTTP_404_NOT_FOUND
+
+    def get(self, request, pk):
+        song_by_id = self.get_object(pk)
+        serializer = SongSerializer(song_by_id)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        song_info_update = self.get_object(pk)
+        serializer = SongSerializer(song_info_update, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        song_info_delete = self.get_object(pk)
+        song_info_delete.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
